93de50e5ea533478b53bf701ce5247ea7a5ff7f4
melih <melih@MELIH>
2024-12-18 23:30:44 +0300

    Proje dosyası ekle.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Azure;
+using BerberSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BerberSite.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+using BerberSite.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// HttpContextAccessor hizmetini ekleyin
+builder.Services.AddHttpContextAccessor();
+
+// DB Baðlantýsý
+builder.Services.AddDbContext<BerberSite.Data.ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// MVC servislerini ekleyin
+builder.Services.AddControllersWithViews();
+
+// Session
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+// Middleware
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseSession();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+}
+
+app.Run();
